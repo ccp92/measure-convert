b4a1fcd5a92bc509d3a0d0e278c9c114135e9c8c
Chris Parsons <mail@chrisparsons.dev>
2022-02-03 23:00:11 +0000

    Rename measure to temperatureUnit

--- src/temperatures/index.test.ts
+++ src/temperatures/index.test.ts
@@ -1,25 +1,25 @@
-import temperatures, { measures } from "./index";
+import temperatures, { temperatureUnit } from "./index";
 
 describe("temperatures", () => {
   it("should return the correct celsius value when given fahrenheit", () => {
-    expect(temperatures(32, measures.fahrenheit, measures.celsius)).toBe(0);
-    expect(temperatures(212, measures.fahrenheit, measures.celsius)).toBe(100);
+    expect(temperatures(32, temperatureUnit.fahrenheit, temperatureUnit.celsius)).toBe(0);
+    expect(temperatures(212, temperatureUnit.fahrenheit, temperatureUnit.celsius)).toBe(100);
   });
 
   it("should return the correct fahrenheit value when given celsius", () => {
-    expect(temperatures(0, measures.celsius, measures.fahrenheit)).toBe(32);
-    expect(temperatures(100, measures.celsius, measures.fahrenheit)).toBe(212);
+    expect(temperatures(0, temperatureUnit.celsius, temperatureUnit.fahrenheit)).toBe(32);
+    expect(temperatures(100, temperatureUnit.celsius, temperatureUnit.fahrenheit)).toBe(212);
   });
 
   it("should return an error if both measures are celsius", () => {
-    expect(() => temperatures(0, measures.celsius, measures.celsius)).toThrow(
+    expect(() => temperatures(0, temperatureUnit.celsius, temperatureUnit.celsius)).toThrow(
       "Cannot convert from celsius to celsius"
     );
   });
 
   it("should return an error if both measures are fahrenheit", () => {
     expect(() =>
-      temperatures(0, measures.fahrenheit, measures.fahrenheit)
+      temperatures(0, temperatureUnit.fahrenheit, temperatureUnit.fahrenheit)
     ).toThrow("Cannot convert from fahrenheit to fahrenheit");
   });
 });
--- src/temperatures/index.ts
+++ src/temperatures/index.ts
@@ -1,20 +1,20 @@
-export const enum measures {
+export const enum temperatureUnit {
   celsius = "celsius",
   fahrenheit = "fahrenheit",
 }
 
 const temperatures = (
   value: number,
-  sourceMeasurement: measures,
-  targetMeasurement: measures
+  sourceMeasurement: temperatureUnit,
+  targetMeasurement: temperatureUnit
 ) => {
   const isFahrenheitToCelsius =
-    sourceMeasurement === measures.fahrenheit &&
-    targetMeasurement === measures.celsius;
+    sourceMeasurement === temperatureUnit.fahrenheit &&
+    targetMeasurement === temperatureUnit.celsius;
 
   const isCelsiusToFahrenheit =
-    sourceMeasurement === measures.celsius &&
-    targetMeasurement === measures.fahrenheit;
+    sourceMeasurement === temperatureUnit.celsius &&
+    targetMeasurement === temperatureUnit.fahrenheit;
 
   if (isFahrenheitToCelsius) {
     return ((value - 32) * 5) / 9;
